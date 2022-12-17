30770a87cd2884ef44e108e1e4d051a835b94c4a
Kaan Turgute <kaan.turgut@deop.ca>
2022-12-17 13:06:42 -0500

    initial commit

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "test_rg"
-  location = "canadacentral"
+resource "azurerm_resource_group" "api_rg" {
+  name     = var.rg_name
+  location = var.location
 }
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "rg_name" {
+    type = string
+    default = "test"  
+}
+variable "location" {
+    type = string
+  default = "canadacentral"
+}
