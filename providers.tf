ffaad534fc3a3049eced301a971d1ba7031e092a
Kaan Turgute <kaan.turgut@deop.ca>
2022-12-17 11:50:22 -0500

    initial commit

---
+++ providers.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.36.0"
+    }
+  }
+}
+
+backend "remote" {
+organization = "deopkaan"
+
+workspaces {
+  name = "terraform_cloud_test_Dec17"
+}
+}
+
+
+provider "azurerm" {
+  features{ 
+  }
+}
